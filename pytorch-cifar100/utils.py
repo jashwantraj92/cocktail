5ee9c3bdc2007327c7ef47dfa4b83d019c5f5ac9
root <root@p100.novalocal>
2021-02-08 14:29:34 +0000

    cifar 100 files

--- pytorch-cifar100/utils.py
+++ pytorch-cifar100/utils.py
@@ -208,10 +208,11 @@ def get_test_dataloader(mean, std, batch_size=16, num_workers=2, shuffle=True):
     ])
     #cifar100_test = CIFAR100Test(path, transform=transform_test)
     cifar100_test = torchvision.datasets.CIFAR100(root='./data', train=False, download=True, transform=transform_test)
+    print (cifar100_test.classes)
     cifar100_test_loader = DataLoader(
         cifar100_test, shuffle=shuffle, num_workers=num_workers, batch_size=batch_size)
 
-    return cifar100_test_loader
+    return cifar100_test_loader, cifar100_test.classes
 
 def compute_mean_std(cifar100_dataset):
     """compute the mean and std of cifar100 dataset
